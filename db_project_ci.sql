bd7f4621fdf4ad0d64a978b22cf0b9df2528d45f
firdanuzula <fnuzula17112005@gmail.com>
2026-03-27 00:49:42 +0700

    edit db

--- db_project_ci.sql
+++ db_project_ci.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Mar 23, 2026 at 03:15 PM
--- Server version: 8.4.3
--- PHP Version: 8.3.26
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `db_project_ci`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `produk`
---
-
 CREATE TABLE `produk` (
   `id` int NOT NULL,
   `nama_produk` varchar(255) NOT NULL,
@@ -34,11 +5,7 @@ CREATE TABLE `produk` (
   `email_kontak` varchar(100) NOT NULL,
   `gambar` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `produk`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `produk` (`id`, `nama_produk`, `deskripsi`, `email_kontak`, `gambar`, `created_at`) VALUES
 (6, 'buku', 'buku python', 'adminlab@gmail.com', '1bd397437d99fc77fcf29c4fc61063a8.jpg', '2026-03-10 14:20:39'),
@@ -47,58 +14,28 @@ INSERT INTO `produk` (`id`, `nama_produk`, `deskripsi`, `email_kontak`, `gambar`
 (9, 'router', 'tp-link', 'adminlab@gmail.com', '0962fcc2a5c9fc87eaeeda62d8e2a33b.jpg', '2026-03-23 15:06:26'),
 (10, 'cpu', 'corei9 \r\nX-Series', 'adminlab@gmail.com', 'ca369dc060d3dfedfb4faab4be42b27b.jpg', '2026-03-23 15:08:03');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(2, 'habiba', '$2y$10$tK8iwAYxOmr.Iroe7WZFd.YpbOg4lR6fbn19yJA7cqMBCmeprdaPO');
+(2, 'habiba', '$2y$10$tK8iwAYxOmr.Iroe7WZFd.YpbOg4lR6fbn19yJA7cqMBCmeprdaPO'),
+(0, 'f', '$2y$10$Huo9R6PqLovGwZPyKsyjc.vPh5qEzyoEtiDrhIloy5H/3K4.hxAvS');
+COMMIT;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `produk`
---
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `produk`
---
 ALTER TABLE `produk`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
